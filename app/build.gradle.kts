baf8a889104022d8ca62d891465811eb5588fc33
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-01-27 00:55:00 +0300

    Project colors added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.huseyinozkan.quizonline"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.huseyinozkan.quizonline"
