740baf249635fdb5eff08bd509d7f1533dda6a37
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-01-28 16:16:08 +0300

    Details UI created

--- app/src/main/java/com/huseyinozkan/quizonline/MainActivity.kt
+++ app/src/main/java/com/huseyinozkan/quizonline/MainActivity.kt
@@ -2,10 +2,30 @@ package com.huseyinozkan.quizonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.huseyinozkan.quizonline.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var quizModelList: MutableList<QuizModel>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quizModelList = mutableListOf()
+        getDataFromFirebase()
+
+    }
+
+    private fun setupRecyclerView(){
+
+    }
+    private fun getDataFromFirebase(){
+        quizModelList.add(QuizModel("1","Programming","All the basic programming","10"))
+        quizModelList.add(QuizModel("2","Computer","All the computer questions","20"))
+        quizModelList.add(QuizModel("3","Georaphy","boost your Georaphic knowledge","15"))
+        setupRecyclerView()
     }
 }
